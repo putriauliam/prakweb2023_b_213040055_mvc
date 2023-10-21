4ab9d819c0986485b72c68aa91aa1c808b3e2a91
putriauliam <ptriauliam61@gmail.com>
2023-10-22 01:13:42 +0700

    MVC Video 7 - Model

---
+++ app/models/User_model.php
@@ -0,0 +1,10 @@
+<?php  
+
+class User_model {
+    private $nama = 'Putri';
+
+    public function getUser()
+    {
+        return $this->nama;
+    }
+}
--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,9 +1,9 @@
 <div class="container">
     <div class="jumbotron mt-4">
         <h1 class="display-4">Selamat Datang di Website Saya</h1>
-        <p class="lead">Website ini dibangun menggunakan arsitektur Model-View-Controller (MVC) dengan bahasa pemrograman PHP</p>
+        <p class="lead">Halo, nama saya <?= $data['nama'];?></p>
         <hr class="my-4">
-        <p>PHP MVC adalah singkatan dari "Model-View-Controller" dalam pengembangan web menggunakan bahasa pemrograman PHP. Ini adalah sebuah pendekatan arsitektur yang digunakan untuk memisahkan komponen utama dari sebuah aplikasi web. MVC dapat mempercepat pengembangan.....</p>
-        <a class="btn btn-primary btn-lg" href="#" role="button">Selengkapnya</a>
+        <p>it uses utility classes for typography and spacing to space content out within the larger container.</p>
+        <a class="btn btn-primary btn-lg" href="#" role="button">Learn More</a>
     </div>
 </div>
