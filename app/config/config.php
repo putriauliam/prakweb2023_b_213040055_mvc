eb194db7f1bbcfee5ceb18f04ddf0b3b708a00b2
putriauliam <ptriauliam61@gmail.com>
2023-10-22 03:19:17 +0700

    MVC Video 8 - Database Wrapper

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php  
+
+define('BASEURL', 'http://localhost/prakweb2023_b_213040055_mvc/public');
+
+// DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'phpmvc');
--- app/core/Constants.php
+++
@@ -1,3 +0,0 @@
-<?php  
-
-define('BASEURL', 'http://localhost/prakweb2023_b_213040055_mvc/public');
