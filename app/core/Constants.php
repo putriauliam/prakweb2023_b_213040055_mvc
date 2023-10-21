05c148c99e0cd790efc6ad208b395c9d8c48365d
putriauliam <ptriauliam61@gmail.com>
2023-10-21 18:08:15 +0700

    MVC Video 6 - Assets

---
+++ app/core/Constants.php
@@ -0,0 +1,3 @@
+<?php  
+
+define('BASEURL', 'http://localhost/prakweb2023_b_213040055_mvc/public');
--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,2 +1,9 @@
-
-    <h1>Selamat Datang di Website Saya</h1>
+<div class="container">
+    <div class="jumbotron mt-4">
+        <h1 class="display-4">Selamat Datang di Website Saya</h1>
+        <p class="lead">Website ini dibangun menggunakan arsitektur Model-View-Controller (MVC) dengan bahasa pemrograman PHP</p>
+        <hr class="my-4">
+        <p>PHP MVC adalah singkatan dari "Model-View-Controller" dalam pengembangan web menggunakan bahasa pemrograman PHP. Ini adalah sebuah pendekatan arsitektur yang digunakan untuk memisahkan komponen utama dari sebuah aplikasi web. MVC dapat mempercepat pengembangan.....</p>
+        <a class="btn btn-primary btn-lg" href="#" role="button">Selengkapnya</a>
+    </div>
+</div>
